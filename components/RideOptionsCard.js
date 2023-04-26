bd53cce447c45b45b18992f29570f9e97c84b82c
aritra <chatterjee.aritra@protonmail.com>
2023-04-26 12:14:19 +0530

    Map Commit

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const RideOptionsCard = () => {
+  return (
+    <View>
+      <Text>Pick a ride </Text>
+    </View>
+  );
+};
+
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
